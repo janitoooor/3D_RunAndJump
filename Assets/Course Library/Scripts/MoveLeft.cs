c3e763723d7a05727120c58d57a353dc4e868c39
Vitaly <59324718+janitoooor@users.noreply.github.com>
2023-02-25 13:25:25 +0300

    Update MoveLeft.cs

--- Assets/Course Library/Scripts/MoveLeft.cs
+++ Assets/Course Library/Scripts/MoveLeft.cs
@@ -4,35 +4,31 @@ using UnityEngine;
 
 public class MoveLeft : MonoBehaviour
 {
-    public float speed = 35.0f;
-    private float leftBound = -15.0f;
+    [SerializeField] private float _speed = 35.0f;
+    private float _leftBound = -15.0f;
 
     private PlayerController playerControllerScript;
 
-    // Start is called before the first frame update
     void Start()
     {
-        // find the player object that has its PlayerController component
         playerControllerScript = GameObject.Find("Player").GetComponent<PlayerController>();
     }
-
-    // Update is called once per frame
+    
     void Update()
     {
        if(playerControllerScript.gameOver == false)
         {
             if(playerControllerScript.rushUp)
             {
-                transform.Translate(Vector3.left * Time.deltaTime * (speed * 2));
+                transform.Translate(Vector3.left * Time.deltaTime * (_speed * 2));
             }
             else
             {
-                transform.Translate(Vector3.left * Time.deltaTime * speed);
+                transform.Translate(Vector3.left * Time.deltaTime * _speed);
             }
         }
-       if(transform.position.x < leftBound && gameObject.CompareTag("Obstacle"))
-        {
+        
+       if(transform.position.x < _leftBound && gameObject.CompareTag("Obstacle"))
             Destroy(gameObject);
-        }
     }
 }
